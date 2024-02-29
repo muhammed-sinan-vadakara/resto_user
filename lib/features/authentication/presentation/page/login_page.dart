f499100da24eb42652901b4b2c77f95db55301c5
muhammed-sinan-vadakara <muhammedsinan5034@gmail.com>
2024-02-29 05:58:08 +0530

    updated

--- lib/features/authentication/presentation/page/login_page.dart
+++ lib/features/authentication/presentation/page/login_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  static const routePath = "/loginPage";
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }
