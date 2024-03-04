f8163eca2d9f83f9be0b94980016e917a49ec896
epashiq <155516430+epashiq@users.noreply.github.com>
2024-03-04 11:02:59 +0530

    Core creation (#3)
    * setting Themdata
    
    * setting Themdata
    
    * modified main page
    
    * core updated
    
    * cart Page modified

--- lib/core/dependencies/setup_dependencies.dart
+++ lib/core/dependencies/setup_dependencies.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
 import 'package:resto_user/core/router/router.dart';
-import 'package:resto_user/core/themes/dark_theme.dart';
 import 'package:resto_user/core/themes/light_theme.dart';
 
 final getIt = GetIt.instance;
@@ -11,5 +10,4 @@ void setupDependencies() {
   /// Router dependencies
   getIt.registerSingleton<GoRouter>(router);
   getIt.registerSingleton<ThemeData>(lightTheme);
-  getIt.registerSingleton<ThemeData>(darkTheme);
 }
