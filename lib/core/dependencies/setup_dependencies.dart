5246ddd71c7b8fa01437b7b5e3b4dfa5183bc6ea
shamilclm <157091973+shamilclm@users.noreply.github.com>
2024-03-04 12:29:34 +0530

    Shamil map (#4)
    * starting
    
    * stating ui
    
    * map bottom sheet completed

--- lib/core/dependencies/setup_dependencies.dart
+++ lib/core/dependencies/setup_dependencies.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:go_router/go_router.dart';
+import 'package:resto_user/core/app_assets/app_asset_constant.dart';
 import 'package:resto_user/core/router/router.dart';
 import 'package:resto_user/core/themes/light_theme.dart';
 
@@ -10,4 +11,5 @@ void setupDependencies() {
   /// Router dependencies
   getIt.registerSingleton<GoRouter>(router);
   getIt.registerSingleton<ThemeData>(lightTheme);
+  getIt.registerSingleton<AppAssetConstants>(AppAssetConstants());
 }
---
+++ lib/features/map/presentation/widgets/map_bottonsheet_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get_it/get_it.dart';
+import 'package:resto_user/core/app_assets/app_asset_constant.dart';
+import 'package:resto_user/core/themes/app_theme.dart';
+
+void viewModelBottomSheet(BuildContext context) {
+  // WidgetsBinding.instance.addPostFrameCallback((_) {
+  final theme = AppTheme.of(context).typography;
+  final space = AppTheme.of(context).spaces;
+  final color = AppTheme.of(context).colors;
+  final assets = GetIt.I.get<AppAssetConstants>();
+  showModalBottomSheet(
+    context: context,
+    builder: (context) => Container(
+      height: 400,
+      width: MediaQuery.sizeOf(context).width,
+      child: Column(
+        children: [
+          SizedBox(
+            height: space.space_400,
+          ),
+          SvgPicture.asset(assets.icLocation),
+          SizedBox(
+            height: space.space_500,
+          ),
+          Text(
+            "Device location not enabled",
+            style: theme.h700,
+          ),
+          SizedBox(
+            height: space.space_100,
+          ),
+          Text(
+            "Enable your device location for a better\n                   delivery experience",
+            style: theme.h300,
+          ),
+          SizedBox(
+            height: space.space_600,
+          ),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  shape: ContinuousRectangleBorder(
+                      borderRadius: BorderRadius.circular(space.space_150)),
+                  backgroundColor: color.primary,
+                  minimumSize: Size(space.space_700 * 6, space.space_600)),
+              onPressed: () {},
+              child: Text("Enable location permission",
+                  style: theme.h300.copyWith(color: color.secondary))),
+          SizedBox(
+            height: space.space_400,
+          ),
+          TextButton(
+              onPressed: () {},
+              child: Text("Search location manually",
+                  style: theme.h300.copyWith(
+                    color: color.primary,
+                  ))),
+        ],
+      ),
+    ),
+  );
+}
