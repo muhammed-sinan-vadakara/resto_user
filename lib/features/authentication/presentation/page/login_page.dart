ed3408f8e3755e74b79793c4ca8a601d8c8f6732
muhammed-sinan=vadakara <muhammedsinanvadakara@gmail.com>
2024-03-05 11:20:06 +0530

    exception added

--- lib/features/authentication/presentation/page/login_page.dart
+++ lib/features/authentication/presentation/page/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:resto_user/features/authentication/presentation/widgets/elavated_Button_widget.dart';
 
 class LoginPage extends StatelessWidget {
   static const routePath = "/loginPage";
@@ -17,6 +18,15 @@ class LoginPage extends StatelessWidget {
             )
           ],
         ),
+      ),
+      bottomNavigationBar: Column(
+        children: [
+          ElevatedButtonWidget(
+            text: "Send OTP",
+            onPressed: () {},
+          ),
+          ElevatedButtonWidget(text: "", onPressed: () {})
+        ],
       ),
     );
   }
--- lib/features/authentication/presentation/page/otp_verify_page.dart
+++ lib/features/authentication/presentation/page/otp_verify_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:resto_user/core/themes/app_theme.dart';
+import 'package:resto_user/features/authentication/presentation/widgets/elavated_Button_widget.dart';
 import 'package:resto_user/features/authentication/presentation/widgets/otp_box_widget.dart';
 
 class OtpVerificationPage extends StatelessWidget {
@@ -13,32 +14,55 @@ class OtpVerificationPage extends StatelessWidget {
 
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 24, bottom: 124),
-                child: Container(
-                  child: SvgPicture.asset(
-                      "assets/images/login_background_image.svg"),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 24, bottom: 124),
+                  child: Container(
+                    child: SvgPicture.asset(
+                        "assets/images/login_background_image.svg"),
+                  ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  OtpBoxWidget(theme: theme),
-                  OtpBoxWidget(theme: theme),
-                  OtpBoxWidget(theme: theme),
-                  OtpBoxWidget(theme: theme),
-                  OtpBoxWidget(theme: theme),
-                  OtpBoxWidget(theme: theme),
-                ],
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(right: 270),
+                  child: Text(
+                    "Enter OTP",
+                    style: TextStyle(
+                      color: theme.colors.text,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    OtpBoxWidget(theme: theme),
+                    OtpBoxWidget(theme: theme),
+                    OtpBoxWidget(theme: theme),
+                    OtpBoxWidget(theme: theme),
+                    OtpBoxWidget(theme: theme),
+                    OtpBoxWidget(theme: theme),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 5),
+                      child: Text("00:00"),
+                    ),
+                    TextButton(onPressed: () {}, child: Text("Resend OTP"))
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
+      bottomNavigationBar:
+          ElevatedButtonWidget(text: "Verify OTP", onPressed: () {}),
     );
   }
 }
