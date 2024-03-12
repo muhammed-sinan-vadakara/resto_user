c0cb0c69d29c84df75db853fe2455739a7017694
MuhammedFaasil <muhammedfaasilpty@gmail.com>
2024-03-12 11:55:33 +0530

    model class created for getting data from firestore

---
+++ lib/features/checkout/data/model/coupon_model.dart
@@ -0,0 +1,38 @@
+
+// ignore_for_file: unused_element
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:resto_user/core/enums/coupon_type.dart';
+
+part 'coupon_model.freezed.dart';
+part 'coupon_model.g.dart';
+
+@freezed
+class CouponModel with _$CouponModel {
+   const CouponModel._();
+  factory CouponModel({
+    required String? id,
+    required String? title,
+    required String code,
+    required CouponType couponType,
+    required double percentageOrAmount,
+    required List<String> condition,
+  }) = _CouponModel;
+
+    factory CouponModel.fromJson(Map<String, dynamic> json) =>
+      _$CouponModelFromJson(json);
+
+  factory CouponModel.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) 
+  {
+    final data = snapshot.data()!;
+    data['id'] = snapshot.id;
+    return CouponModel.fromJson(data);
+  }
+  Map<String, dynamic> toFirestore() {
+    return toJson()..remove('id');
+  }
+}
---
+++ lib/features/checkout/data/model/coupon_model.g.dart
@@ -0,0 +1,34 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'coupon_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$CouponModelImpl _$$CouponModelImplFromJson(Map<String, dynamic> json) =>
+    _$CouponModelImpl(
+      id: json['id'] as String?,
+      title: json['title'] as String?,
+      code: json['code'] as String,
+      couponType: $enumDecode(_$CouponTypeEnumMap, json['couponType']),
+      percentageOrAmount: (json['percentageOrAmount'] as num).toDouble(),
+      condition:
+          (json['condition'] as List<dynamic>).map((e) => e as String).toList(),
+    );
+
+Map<String, dynamic> _$$CouponModelImplToJson(_$CouponModelImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'code': instance.code,
+      'couponType': _$CouponTypeEnumMap[instance.couponType]!,
+      'percentageOrAmount': instance.percentageOrAmount,
+      'condition': instance.condition,
+    };
+
+const _$CouponTypeEnumMap = {
+  CouponType.amount: 'amount',
+  CouponType.percentage: 'percentage',
+  CouponType.freeDelivery: 'freeDelivery',
+};
