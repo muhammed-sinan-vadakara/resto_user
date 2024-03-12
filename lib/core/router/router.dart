7246f2f8c3a51fbdfbbb52bc80111f39b1f35f95
shamilclm <157091973+shamilclm@users.noreply.github.com>
2024-03-12 11:12:13 +0530

    map current location completed (#9)
    * map current location completed
    
    * resolved issues
    
    * resolved issues

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,10 +1,11 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:resto_user/features/cart/presentation/pages/cart_page.dart';
+import 'package:resto_user/features/map/presentation/page/map_page.dart';
+import 'package:resto_user/features/profile/presentation/pages/profile_page.dart';
 import 'package:resto_user/features/checkout/presentation/pages/checkout_page.dart';
 import 'package:resto_user/features/home/presentation/bloc/category_bloc/category_bloc.dart';
 import 'package:resto_user/features/home/presentation/pages/home_page.dart';
-import 'package:resto_user/features/profile/presentation/pages/profile_page.dart';
 
 final router = GoRouter(
   initialLocation: HomePage.routPath,
@@ -12,6 +13,10 @@ final router = GoRouter(
     GoRoute(
       path: CartPage.routPath,
       builder: (context, state) => const CartPage(),
+    ),
+    GoRoute(
+      path: Map.routePath,
+      builder: (context, state) => const Map(),
     ),
     GoRoute(
       path: ProfilePage.routePath,
