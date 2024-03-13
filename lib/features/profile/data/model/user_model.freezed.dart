1cd6a60ef58c0e3a7b114a61448f755beb40c8b3
MuhammedFaasil <muhammedfaasilpty@gmail.com>
2024-03-13 09:57:43 +0530

    model class created for coupons

--- lib/features/profile/data/model/user_model.freezed.dart
+++ lib/features/profile/data/model/user_model.freezed.dart
@@ -1,6 +1,6 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: type=lint
+// ignore_for_file: type=lint, duplicate_ignore
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'user_model.dart';
@@ -21,7 +21,7 @@ UserModel _$UserModelFromJson(Map<String, dynamic> json) {
 /// @nodoc
 mixin _$UserModel {
   String get imgPath =>
-      throw _privateConstructorUsedError; // ignore: invalid_annotation_target
+      throw _privateConstructorUsedError; // ignore: 
   @JsonKey(name: 'user_name')
   String get userName => throw _privateConstructorUsedError;
 
