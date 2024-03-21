d20d02c4b95cd7f5d2a73cc339ce58c48b3434d4
muhammed-sinan=vadakara <muhammedsinanvadakara@gmail.com>
2024-03-21 13:31:24 +0530

    Firebase Storage and fireStore added

--- lib/features/authentication/domian/usecases/details_add_usecase.dart
+++ lib/features/authentication/domian/usecases/details_add_usecase.dart
@@ -4,9 +4,9 @@ import 'package:resto_user/core/exceptions/base_exception/base_exception.dart';
 import 'package:resto_user/features/authentication/domian/entity/details_add_entity.dart';
 import 'package:resto_user/features/authentication/domian/repositories/auth_repository.dart';
 
-final class AddOfferUseCase {
+final class AddDetailsUseCase {
   final AuthRepository repository;
-  AddOfferUseCase({required this.repository});
+  AddDetailsUseCase({required this.repository});
   Future<void> call({
     required String imagePath,
     required String id,
